6ac00390ac62f80074e3dcb063243969d2ddc65a
RoachOSL <os1996@o2.pl>
2024-03-01 13:39:37 +0100

    First commit

---
+++ src/main/java/com/quickvocabcheck/QuickVocabCheckApplication.java
@@ -0,0 +1,13 @@
+package com.quickvocabcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickVocabCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickVocabCheckApplication.class, args);
+    }
+
+}
